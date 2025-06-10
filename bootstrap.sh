671ed1d0dad826987e213d7a249b4cf76833bf5a
Chris W Hendricks <chris.hendricks@tylertech.com>
2025-06-10 11:47:37 -0400

    Update

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,4 +1,7 @@
-echo "Executing Bootstrap" > ~/dotfiles-bootstrap.log
+ghuser=$1
+ghemail=$2
+
+echo "Executing Bootstrap $1 - $2 " > ~/dotfiles-bootstrap.log
 
 echo "Github Username is $GITHUB_AUTHOR_NAME" >> ~/dotfiles-bootstrap.log
 echo "Github Email is $GITHUB_AUTHOR_EMAIL" >> ~/dotfiles-bootstrap.log
