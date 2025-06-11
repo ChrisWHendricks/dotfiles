4418a66195f2f28713ace49ddd417443d8016737
Chris W Hendricks <chris.hendricks@tylertech.com>
2025-06-11 14:59:47 -0400

    fix: update linux install scripts

--- linux/install.sh
+++ linux/install.sh
@@ -3,5 +3,29 @@ if test ! "$(uname)" = "Linux"
   exit 0
 fi
 
+# Logging functions to make messages pretty
+info() {
+    printf "\r  [ \033[00;34m..\033[0m ] $1\n"
+    
+}
+
+user() {
+    printf "\r  [ \033[0;33m??\033[0m ] $1\n"
+   
+}
+
+success() {
+    printf "\r\033[2K  [ \033[00;32mOK\033[0m ] $1\n"
+   
+}
+
+fail() {
+    printf "\r\033[2K  [\033[0;31mFAIL\033[0m] $1\n"
+    echo ''
+    exit
+}
+
 echo "Installing Linux Apps"
+info "Installing applications from Brewfile..."
+brew bundle --file=$HOME/.dotfiles/linux/Brewfile
 
